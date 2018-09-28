755b7f34d739c7074eaa5139e28a851e5bcf3935
Endestral <josephmurtha@gmail.com>
2018-09-27 19:13:54 -0500

    project 1 main

--- Application.java
+++ Application.java
@@ -1,37 +1,61 @@
+import java.util.Scanner;
+import java.awt.*;
 public class Application {
-    public static boolean testAdd(Complex3 first, Complex3 second, double realExpected, double imagExpected) { //testadd( firstcomplex, second complex, real expected, imaginary expected)
-        first.addComplex(second);
-        double realActual = first.getRealPart();
-        double imagActual = first.getImaginaryPart();
-        return realExpected == realActual && imagExpected==imagActual;
+    public Canvas drawing = new Canvas(1600,1600);
 
+    public void triFractal(int n, double h, double w, double posX, double posY) {
+        Triangle myTriangle = new Triangle(posX,posY,w,h);
+        if (n ==1) {
+            drawing.drawShape(myTriangle);
+        }
+        else {
+            drawing.drawShape(myTriangle);
+
+            //move to bottom left;
+            triFractal(n-1,.5*h,.5*w, myTriangle.getXPos() -.25 * myTriangle.getWidth(), myTriangle.getYPos() - .5 * myTriangle.getHeight());
+
+            //move to bottom right
+            triFractal(n-1,.5*h,.5*w, myTriangle.getXPos() + myTriangle.getWidth() -.25 * myTriangle.getWidth(), myTriangle.getYPos() - .5 * myTriangle.getHeight());
+            //move to top middle
+            triFractal(n-1,.5*h,.5*w, myTriangle.getXPos() - .5*myTriangle.getWidth(), myTriangle.getYPos() - myTriangle.getHeight());
+
+        }
     }
 
-    public static boolean testSub(Complex3 first, Complex3 second, double realExpected, double imagExpected) { //testSub)firstcomplex, second complex, expected)
-        first.subtractComplex(second);
-        double realActual = first.getRealPart();
-        double imagActual = first.getImaginaryPart();
-        return realExpected == realActual && imagExpected==imagActual;
+    public void recFractal(int n, double l, double posX, double posY) {
+        Rectangle myRectangle = new Rectangle(posX,posY,l,l);
+        if (n ==1) {
+            drawing.drawShape(myRectangle);
+        }
+        else {
+            drawing.drawShape(myRectangle);
+            //move to bottom left;
+            recFractal(n-1,.5 * l,myRectangle.getXPos() - .5 * l, myRectangle.getYPos() + l);
+            //move to bottom right
+            recFractal(n-1,.5 * l, myRectangle.getXPos() + l,myRectangle.getYPos() - .5 * l);
+            //move to top right
+            recFractal(n-1, .5 * l,myRectangle.getXPos() + l, myRectangle.getYPos() + myRectangle.getHeight());
+            //move to top left
+            recFractal(n-1,.5 * l, myRectangle.getXPos() - .5 * l, myRectangle.getYPos() - .5 * l);
+
+        }
+
     }
     public static void main(String[] args) {
-        //init c
-        Complex3 A = new Complex3(5, 6);
-        Complex3 B = new Complex3(2,2);
-        //test for complex add
-        System.out.println(testAdd(A,B,7,8));
-        //Test for Complex sub
+        Application fract = new Application();
+        //ask user for input
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the shape you would like to draw (Circle, Triangle, Rectangle):");
+        //String shapeRequest = sc.next();
 
-        System.out.println(testSub(A,B,3,4));
-        //new inputs
-        A.setRealPart(9);
-        A.setImaginaryPart(3);
-        B.setRealPart(6);
-        B.setImaginaryPart(1);
-        //test for new complex add
-        System.out.println(testAdd(A,B,15,4));
-        //test for new complex sub
-        System.out.println(testSub(A,B,3,2));
+        //System.out.println("Enter the color you would like your shapes to be");
+        //String shapeColor = sc.next();
+        //test print
+        //System.out.println(shapeRequest + " " + iterRequest);
+        //draw number of patterns user asked for
 
-
+        fract.triFractal(7,400,400,800,800);
+        //fract.recFractal(7,100,400,400);
+        //compute total area of drawn patterns.
     }
 }
